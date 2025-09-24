7f7fa7e5e955d625b0b194377ed349abf6692f8f
SitcomReality <SitcomReality@websim.ai>
2025-09-24 15:31:43 +0000

    v325: Please finish implementing the loading_update.md

--- render/index.js
+++ render/index.js
@@ -2,10 +2,10 @@
  * Render module exports
  */
 export { RenderManager } from './RenderManager.js';
-export { Camera } from './systems/Camera.js';
-export { WorldTransform } from './systems/WorldTransform.js';
+export { Camera } from './core/Camera.js';
+export { WorldTransform } from './core/WorldTransform.js';
 export { AnimationLoop } from './systems/AnimationLoop.js';
-export { TextureManager } from './systems/TextureManager.js';
+export { TextureManager } from './core/TextureManager.js';
 export { ParticleSystem } from './systems/ParticleSystem.js';
 export { Nameplate } from './ui/Nameplate.js';
 export { TrackRenderer } from './renderers/TrackRenderer.js';
@@ -14,4 +14,3 @@ export { FerretRenderer } from './renderers/FerretRenderer.js';
 export { FerretAnimationSystem } from './renderers/FerretAnimationSystem.js';
 export { FerretBodyRenderer } from './renderers/FerretBodyRenderer.js';
 export { FerretEyeRenderer } from './renderers/FerretEyeRenderer.js';
-
